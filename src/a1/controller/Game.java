600a079c0718f75f2c34fd6a3a439c1822ef0154
Victor Ignatenkov <vetekm@yahoo.com>
2015-03-05 19:24:47 -0800

    Create GUI components.
    Ignore all .class files.

--- src/a1/controller/Game.java
+++ src/a1/controller/Game.java
@@ -2,21 +2,140 @@ package a1.controller;
 
 import a1.model.GameWorld;
 
+import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
 import java.util.Scanner;
 
 /**
  * Created by Victor Ignatenkov on 2/9/15.
  */
-public class Game {
+public class Game extends JFrame{
 
 
     private GameWorld gw;
 
     public Game() {
+        super("My Racing Game");
+
         gw = new GameWorld();
         gw.initLayout();
-        play();
 
+
+
+
+
+        setLayout(new BorderLayout());
+
+
+
+
+        //**********************************************
+        //              top panel                     **
+        //**********************************************
+
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.LEFT,16,0));
+        topPanel.setBorder(new LineBorder(Color.blue,2));
+        this.add(topPanel,BorderLayout.NORTH);
+
+        JLabel currentTime = new JLabel("Time: ");
+        topPanel.add(currentTime);
+
+
+        JLabel livesLeft = new JLabel("Lives Left: ");
+        topPanel.add(livesLeft);
+
+        JLabel highestPylon = new JLabel("Highest Player Pylon: ");
+        topPanel.add(highestPylon);
+
+        JLabel remainingFuelLevel = new JLabel("Player Fuel Remaining: ");
+        topPanel.add(remainingFuelLevel);
+
+        JLabel playerDamageLevel = new JLabel("Player Damage Level: ");
+        topPanel.add(playerDamageLevel);
+
+        JLabel soundStatus = new JLabel("Sound: ");
+        topPanel.add(soundStatus);
+
+
+        //**********************************************
+        //              left panel                    **
+        //**********************************************
+        JPanel leftPanel = new JPanel();
+        leftPanel.setBorder(new TitledBorder(" Options: "));
+        leftPanel.setLayout(new GridLayout(10,1));
+
+        leftPanel.setBorder(new LineBorder(Color.green,2));
+        this.add(leftPanel, BorderLayout.WEST);
+
+        JButton collideWithNPC = new JButton("Collide With NPC");
+        leftPanel.add(collideWithNPC);
+
+        JButton collideWithPylon = new JButton("Collide With Pylon");
+        leftPanel.add(collideWithPylon);
+
+        JButton collideWithBird = new JButton("Collide With Bird");
+        leftPanel.add(collideWithBird);
+
+        JButton pickUpFuelCan = new JButton("Picked Up FuelCan");
+        leftPanel.add(pickUpFuelCan);
+
+        JButton enterOilSlick = new JButton("Entered Oil Slick");
+        leftPanel.add(enterOilSlick);
+
+        JButton exitOilSlick = new JButton("Exited Oil Slick");
+        leftPanel.add(exitOilSlick);
+
+        JButton switchStrategy = new JButton("Switch Strategy");
+        leftPanel.add(switchStrategy);
+
+        JButton triggerTick = new JButton("Trigger Tick");
+        leftPanel.add(triggerTick);
+
+        JButton quitTheGame = new JButton("Quit");
+        leftPanel.add(quitTheGame);
+
+
+
+        //**********************************************
+        //              central panel                **
+        //**********************************************
+
+        JPanel centerPanel = new JPanel();
+        centerPanel.setBorder(new EtchedBorder());
+        centerPanel.setBackground(new Color(100,100,123));
+        this.add(centerPanel,BorderLayout.CENTER);
+
+
+        //**********************************************
+        //                    menu                    **
+        //**********************************************
+
+        JMenuBar bar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem mItem = new JMenuItem("1");
+        fileMenu.add(mItem);
+
+        JMenu commandMenu = new JMenu("Commands");
+
+        bar.add(fileMenu);
+        bar.add(commandMenu);
+        this.setJMenuBar(bar);
+
+
+
+
+
+
+        setResizable(false);
+        setSize(800, 400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        play();
 
     }
 
